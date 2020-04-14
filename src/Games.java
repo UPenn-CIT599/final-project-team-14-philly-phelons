dea01739e75fe68d531dd60b5af2ba7b7f843bd3
P0bbn <61028344+P0bbn@users.noreply.github.com>
2020-04-14 18:04:29 -0500

    Created Class
    Created class to create Game objects that are universal in scope across leagues.

--- src/Games.java
+++ src/Games.java
@@ -1,15 +1,31 @@
+/**
+ * 
+ * @author Robby Ballard
+ * Class to create a a game object to be populated by the LeagueReader class.All column data that (initially) seems useful to group project 
+ * concerns is included, but there is much more data contained in the file. Also, data listed below may not be in the order in 
+ * which it is arranged in the csv file.
+ */
 
 public class Games {
 
-    private String season;
-    private String gameType;
-    private String gameDate;
-    private String awayTeam;
-    private String homeTeam;
-    private String awayScore;
-    private String homeScore;
+    private String season; //YYYY representing the year the season began in (i.e. the 2020 Super Bowl game is in the 2019 season because the season began in 2019)
+    private String gameType; //Was the game a playoff game? Different types of games are listed differently in the various leagues
+    private String gameDate; // Date the game was played. Listed as DD-MM-YY
+    private String awayTeam; //3 letter code of the away team. Represented as a number in the NHL
+    private String homeTeam;//3 letter code of the home team. Philadelphia is represented by "4" in the NHL.
+    private String awayScore;//Away team score
+    private String homeScore;//Home team score
     
-    
+    /**
+     * Constructor for the class. Creates an object of the Game. 
+     * @param season
+     * @param gameType
+     * @param gameDate
+     * @param awayTeam
+     * @param homeTeam
+     * @param awayScore
+     * @param homeScore
+     */
     public Games(String season, String gameType, String gameDate, String awayTeam, 
             String homeTeam, String awayScore, String homeScore) {
         this.season = season;
@@ -20,7 +36,9 @@ public class Games {
         this.awayScore = awayScore;
         this.homeScore = homeScore;
     }
-
+/*
+ * Getter methods below return the indicated value for each Game object.
+ */
 
     public String getSeason() {
         return season;
